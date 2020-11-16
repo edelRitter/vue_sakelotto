3dadac8f93eedd8c14a87753154dec695a556272
Isshiki <yohei.isshiki@rakuten.com>
2020-11-16 11:44:27 +0900

    added new files for vue CLI

--- sakelotto/src/App.vue
+++ sakelotto/src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sakeLottoIntro msg="酒みくじ"/>
+    <sakeLottoMain />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import sakeLottoIntro from './components/sakeLotto_intro.vue'
+import sakeLottoMain from './components/sakeLotto_main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    sakeLottoIntro,
+    sakeLottoMain
   }
 }
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+  background: #f1f1f1;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333;
 }
+.wf-hannari { font-family: "Hannari"; }
+.wf-notosansjapanese { font-family: "Noto Sans JP"; }
 </style>
---
+++ sakelotto/src/components/sakeLotto_intro.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="sakeLotto-intro">
+    <div class="sakeLotto-intro-bg">
+      <div class="sakeLotto-banner">
+        <h1 class="wf-notosansjapanese">{{ msg }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SakeLottoIntro',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 32px;
+  letter-spacing: 0.8em;
+}
+.sakeLotto-intro {
+  position: relative;
+  width: 100%;
+}
+.sakeLotto-intro-bg {
+  box-sizing: border-box;
+  position: relative;
+  padding: 12px;
+}
+.sakeLotto-banner {
+  box-sizing: border-box;
+  position: relative;
+  width: 50%;
+  max-width: 960px;
+  margin: 40px auto;
+  padding: 12px;
+  border-radius: 4px;
+  color: #cccccc;
+  background: #ffffff;
+  z-index: 1;
+}
+.sakeLotto-intro-bg::after {
+  content: '';
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+  background: #cccccc;
+  z-index: 0;
+}
+@media only screen and (max-width: 600px) {
+  h1 {
+    font-size: 5vw;
+  }
+}
+</style>
