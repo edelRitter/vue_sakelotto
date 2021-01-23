2c0d1d5f42488d074b57504f10bf8a6442bc9cd4
Isshiki <yohei.isshiki@rakuten.com>
2021-01-23 13:45:32 +0900

    installed vue google analytics via https://webty.jp/staffblog/production/post-2381/

--- sakelotto/src/main.js
+++ sakelotto/src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueSocialSharing from 'vue-social-sharing'
 import Multiselect from 'vue-multiselect'
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
+
 Vue.use(VueSocialSharing);
+Vue.use(VueAnalytics, {
+  id: 'G-KH9VX4GVFJ'
+});
 
 Vue.component('multiselect', Multiselect)
 
